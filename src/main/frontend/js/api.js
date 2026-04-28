f36402f5f97bb6f2eb132e6ebb7c2c5e76291a7a
zhlhlz <1774839354@qq.com>
2026-04-28 21:45:33 +0800

    Revert "fix: make frontend api base configurable (#6)"
    This reverts commit f5c8170240729af2f40c6d389261466d17baa18e.

--- src/main/frontend/js/api.js
+++ src/main/frontend/js/api.js
@@ -1,18 +1,8 @@
 // frontend/js/api.js
 (function (global) {
-    function resolveApiBase(baseURL) {
-        if (baseURL) {
-            return baseURL;
-        }
-        if (global.TOURISM_API_BASE) {
-            return global.TOURISM_API_BASE;
-        }
-        return 'http://localhost:8080/api';
-    }
-
     class API {
         constructor(baseURL) {
-            this.baseURL = resolveApiBase(baseURL);
+            this.baseURL = baseURL || 'http://localhost:8080/api';
         }
 
         async request(endpoint, options = {}) {
@@ -113,7 +103,6 @@
 
     // 保持向下兼容：全局可用 window.api
     global.api = new API();
-    global.getTourismApiBase = resolveApiBase;
     // 也暴露构造器，必要时可：new API('http://x.x.x.x:3000/api')
     global.API = API;
 })(window);
